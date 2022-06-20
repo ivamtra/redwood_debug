94445e0be81f6b5690538e9284c66d916885b7a7
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-06-20 14:11:39 +0000

    questioncell gql

--- web/src/components/QuestionCell/QuestionCell.js
+++ web/src/components/QuestionCell/QuestionCell.js
@@ -4,6 +4,12 @@ export const QUERY = gql`
   query FindQuestionQuery($id: Int!) {
     question: question(id: $id) {
       id
+      title
+      language
+      definition
+      other_info
+      userId
+      createdAt
     }
   }
 `
@@ -17,5 +23,23 @@ export const Failure = ({ error }) => (
 )
 
 export const Success = ({ question }) => {
-  return <Question />
+  return (
+    <div>
+      <div>
+        {/*TODO: vantar virkni á takka */}
+        <button>upvote</button>
+        <button>downvote</button>
+        <p>Number of upvotes:</p>
+        <p>Dagsetning: {question.createdAt}</p>
+        <p>userId: {question.userId} (breytist seinna)</p>
+      </div>
+      <h1>{question.title}</h1>
+      <h3>Setningar:</h3>
+      <h4>Bæta við seinna</h4>
+      <div>
+        <h3>Tungumál: {question.language}</h3>
+        <h3>Aðrar upplýsingar: {question.other_info}</h3>
+      </div>
+    </div>
+  )
 }
