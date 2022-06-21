76eea451c85c3ae73a8cf290c0ff0049f80f667e
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-06-21 16:22:16 +0000

    refactor og comment i sentence form

--- web/src/components/TestForm/TestForm.js
+++ web/src/components/TestForm/TestForm.js
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 
+// Placeholder til að það sýni eina línu í viðmótinu
 const initialList = [
   {
     id: '0',
-    sentence: 'test',
+    sentence: 'placeholder',
   },
 ]
 
-const test = () => {
-  console.log('test')
-}
+let returnList
 
 const TestForm = () => {
   const [idIndex, setIdIndex] = useState(1)
@@ -19,10 +18,12 @@ const TestForm = () => {
   const [isDonePressed, setDonePressed] = useState(false)
   const [isFinishedPressed, setFinishedPressed] = useState(false)
 
+  // Geymir textann
   const onChange = (e) => {
     setTextValue(e.target.value)
   }
 
+  // Bæta við nýju gildi í listann
   const handle = () => {
     setIdIndex(idIndex + 1)
     setTextArray([...textArray, { id: String(idIndex), sentence: textValue }])
@@ -32,29 +33,30 @@ const TestForm = () => {
     console.log(textArray)
   }
 
-  const consoleLog = () => {
-    console.log(finalList)
-  }
-
+  // Þegar ýtt er á done takkann
   // FIXME: Þetta þarf alltaf að keyra tvisvar til að virka
   const onDone = async () => {
     setDonePressed(true)
     let tempList = [...textArray]
     tempList.shift()
     setFinalList([...tempList, { id: String(idIndex), sentence: textValue }])
-    while (textArray.length > 0) textArray.shift()
+    // Uncommenta til að láta viðmótslistann hverfa
+    //while (textArray.length > 0) textArray.shift()
     console.log(finalList)
     console.log('list-length = ' + finalList.length)
     console.log('index = ' + idIndex)
-    setTimeout(() => onFinished, 200)
   }
 
+  // Þegar ýtt er á submit takkann
   const onFinished = () => {
     console.log(finalList)
     if (finalList.length === idIndex) {
       // Gera ehv
       setFinishedPressed(true)
       console.log('success')
+      returnList = finalList
+      console.log(returnList)
+      return returnList
     } else {
       // Gera ehv
       console.log('villa')
@@ -67,13 +69,13 @@ const TestForm = () => {
           {textArray.map((item) => (
             <li key={item.id}>
               <div>
-                <p>{textValue}</p>
+                {/* <p>{textValue}</p> */}
                 <input
                   onChange={onChange}
                   type="textfield"
                   name={item.id}
                   id={item.id}
-                  disabled={item.id < idIndex - 1}
+                  disabled={item.id < idIndex - 1 || isDonePressed}
                   placeholder={item.id}
                 />
                 <button onClick={handle}>+</button>
