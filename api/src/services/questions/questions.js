c536dc03e9abb54952fa10673fe30202132cd8bc
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-06-21 21:21:46 +0000

    find max id i api

--- api/src/services/questions/questions.js
+++ api/src/services/questions/questions.js
@@ -29,6 +29,19 @@ export const deleteQuestion = ({ id }) => {
   })
 }
 
+// Fá countið held þetta virki ekki fyrir questionformið t.d. ef einhver deletar spurningu
+// Þá lækkar countið
+
+//Þetta ætti að gefa max-idið
+export const findMaxId = () => {
+  db.question.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    take: 1,
+  })
+}
+
 export const Question = {
   user: (_obj, { root }) =>
     db.question.findUnique({ where: { id: root.id } }).user(),
