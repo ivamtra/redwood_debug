27db50f2bd056a0d8aaf6581b8520781bcd7da34
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-06-22 15:45:52 +0000

    HomePage breytt í TestPage, QuestionPage er nýja homepage

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -6,18 +6,12 @@ import AnswerCell from 'src/components/AnswerCell/AnswerCell'
 const HomePage = () => {
   return (
     <>
-      <MetaTags title="Home" description="Home page" />
+      <MetaTags title="Test" description="Home page" />
 
       <h1>-------------------------------------------------</h1>
 
-      <h1 className="bg-slate-100 text-slate-700">HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <h1>TestPage</h1>
+
       <h1>-------------------------------------------------</h1>
       <h1>Test Svör</h1>
       <h1>-------------------------------------------------</h1>
