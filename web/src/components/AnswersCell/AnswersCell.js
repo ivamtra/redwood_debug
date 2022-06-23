90f35c5811f97e4aaa6883133b1551efe33b6520
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-06-23 14:15:44 +0000

    answers cell byrjun

---
+++ web/src/components/AnswersCell/AnswersCell.js
@@ -0,0 +1,25 @@
+export const QUERY = gql`
+  query AnswersQuery {
+    answers {
+      id
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ answers }) => {
+  return (
+    <ul>
+      {answers.map((item) => {
+        return <li key={item.id}>{JSON.stringify(item)}</li>
+      })}
+    </ul>
+  )
+}
--- web/src/layouts/TestLayout/TestLayout.js
+++ web/src/layouts/TestLayout/TestLayout.js
@@ -21,7 +21,7 @@ const TestLayout = ({ children, user }) => {
       <h2>
         <Link to={routes.questions()}>Spurningar </Link>
         <Link to={routes.forms()}>Forms </Link>
-        <Link to={routes.test()}>Test </Link>
+        <Link to={routes.test()}>Svör </Link>
       </h2>
       <main>{children}</main>
     </>
