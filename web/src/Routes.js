f821031ca9f8bc9d7c0de930f87a2bba39d305f9
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-06-29 14:55:56 +0000

    tree module bætt við

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -14,8 +14,8 @@ import TestLayout from './layouts/TestLayout/TestLayout'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/comment-test" page={CommentTestPage} name="commentTest" />
       <Set wrap={TestLayout}>
+        <Route path="/comment-test" page={CommentTestPage} name="commentTest" />
         <Route path="/question/{id:Int}" page={QuestionPage} name="question" />
         <Route path="/questions" page={QuestionsPage} name="questions" />
         <Route path="/test" page={HomePage} name="test" />
