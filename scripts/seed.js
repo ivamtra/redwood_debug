ea73d72306c84d9af0bd2ebe796302a0d95ee36b
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-07-07 20:38:57 +0000

    seed script i vinnslu

--- scripts/seed.js
+++ scripts/seed.js
@@ -8,17 +8,10 @@ export default async () => {
     //
     // Update "const data = []" to match your data model and seeding needs
     //
-    const data = [
-      // To try this example data with the UserExample model in schema.prisma,
-      // uncomment the lines below and run 'yarn rw prisma migrate dev'
-      //
-      // { name: 'alice', email: 'alice@example.com' },
-      // { name: 'mark', email: 'mark@example.com' },
-      // { name: 'jackie', email: 'jackie@example.com' },
-      // { name: 'bob', email: 'bob@example.com' },
+    // -------------------------------------------------------------------
 
-      // -------------------------------------------------------------------
-      // USER
+    // --------- USER ------------------------------------------------------
+    const userData = [
       {
         id: 0,
         email: 'Placeholder',
@@ -26,6 +19,7 @@ export default async () => {
         salt: 'salt',
       },
       {
+        id: 1,
         email: 'ivan',
         hashedPassword:
           'f01d1e7dc3eff50d1addc98cd59197d9b65833495704f8083a8eb3994630f41e',
@@ -33,6 +27,70 @@ export default async () => {
       },
       { email: 'test', hashedPassword: 'asdasdjkasdffasdjk', salt: 'ASDA' },
     ]
+    // --------- QUESTION ------------------------------------------------------
+    const questionData = [
+      {
+        id: 0,
+        title: 'Placeholder',
+        language: 'Placehodler',
+        definition: 'Placeholder',
+        userId: 0,
+        rating: 0,
+      },
+      {
+        id: 1,
+        title: 'Spurning 1',
+        language: 'enska',
+        definition: 'Skilgreining',
+        userId: 1,
+        rating: 0,
+      },
+    ]
+
+    // --------- ANSWER ------------------------------------------------------
+    const answerData = [
+      {
+        id: 0,
+        title: 'Placeholder',
+        justification: 'Placeholder',
+        userId: 0,
+        questionId: 0,
+        rating: 0,
+      },
+      {
+        id: 5,
+        title: 'Comment test answer',
+        justification: 'Comment test',
+        userId: 1,
+        questionId: 1,
+        rating: 0,
+      },
+    ]
+
+    // --------- COMMENT -----------------------------------------------------
+
+    const commentData = [
+      {
+        id: 0,
+        body: 'placeholder',
+        userId: 0,
+        parentId: 0,
+        answerId: 0,
+        level: 0,
+        rating: 0,
+      },
+      {
+        body: 'First comment!',
+        userId: 1,
+        parentId: 0,
+        answerId: 5,
+        level: 0,
+        rating: 0,
+      },
+    ]
+
+    // -----------------------------------------------------------------------
+
     console.log(
       "\nUsing the default './scripts/seed.{js,ts}' template\nEdit the file to add seed data\n"
     )
@@ -43,13 +101,46 @@ export default async () => {
       //
       // Change to match your data model and seeding needs
       //
-      data.map(async (data) => {
+      userData.map(async (data) => {
         const record = await db.user.create({ data })
         console.log(record)
       })
     )
+      .then(() => {
+        questionData.map(async (data) => {
+          const record = await db.question.create({ data })
+          console.log(record)
+        })
+      })
+      .then(() => {
+        answerData.map(async (data) => {
+          const record = await db.answer.create({ data })
+          console.log(record)
+        })
+      })
+      .then(() => {
+        commentData.map(async (data) => {
+          const record = await db.answerComment.create({ data })
+          console.log(record)
+        })
+      })
   } catch (error) {
     console.warn('Please define your seed data.')
     console.error(error)
   }
 }
+
+// questionData.map(async (data) => {
+//   const record = await db.question.create({ data })
+//   console.log(record)
+// }),
+
+// answerData.map(async (data) => {
+//   const record = await db.answer.create({ data })
+//   console.log(record)
+// }),
+
+// commentData.map(async (data) => {
+//   const record = await db.answerComment.create({ data })
+//   console.log(record)
+// })
