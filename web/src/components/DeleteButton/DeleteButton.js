263a7289019d4213b2882e25adc0f16c6cffb659
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-07-08 15:52:44 +0000

    delete button byrjaður

---
+++ web/src/components/DeleteButton/DeleteButton.js
@@ -0,0 +1,10 @@
+const DeleteButton = () => {
+  return (
+    <div>
+      <h2>{'DeleteButton'}</h2>
+      <p>{'Find me in ./web/src/components/DeleteButton/DeleteButton.js'}</p>
+    </div>
+  )
+}
+
+export default DeleteButton
