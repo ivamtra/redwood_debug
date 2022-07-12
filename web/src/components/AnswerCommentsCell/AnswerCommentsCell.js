dbb2cabfcdc915450609b61f9d0fce57223f6bcf
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-07-12 16:07:00 +0000

    ansercommentscell cleanup

--- web/src/components/AnswerCommentsCell/AnswerCommentsCell.js
+++ web/src/components/AnswerCommentsCell/AnswerCommentsCell.js
@@ -1,7 +1,5 @@
 import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
-import { node } from 'prop-types'
-
 import AnswerCommentCell from '../AnswerCommentCell'
 
 export const QUERY = gql`
@@ -32,9 +30,11 @@ export const Failure = ({ error }) => (
  * ítrar í gegnum það með DFS
  * til að skila kommentum í rétta röð
  *
+ * Tímaflækja O(n) ef notaður er LinkedList
  */
 const newSortComments = (answerComments) => {
   // Stilla hakkatöflu
+  // Ætti að vera O(n) ef append er O(1)
   const hashMap = new Map()
   answerComments.forEach((item) => {
     try {
@@ -45,6 +45,7 @@ const newSortComments = (answerComments) => {
   })
   console.log(hashMap)
 
+  // Upphafsstilling
   const stack = []
   let currentChildren = hashMap.get(0)
   currentChildren.forEach((child) => {
@@ -54,7 +55,7 @@ const newSortComments = (answerComments) => {
   let currentNode
   let returnList = []
 
-  // Ítra í gegn
+  // Ítra í gegn með DFS
   while (stack.length !== 0) {
     currentNode = stack.pop()
     returnList.push(currentNode)
@@ -72,18 +73,10 @@ const newSortComments = (answerComments) => {
 
 export const Success = ({ answerComments, answerId }) => {
   const [list, setList] = useState([])
-  // const testList = useRef(newSortComments(answerComments))
-
-  //useEffect(() => console.log(answerComments))
 
   useLayoutEffect(() => {
     setList(newSortComments(answerComments))
   }, [answerComments])
-
-  // useEffect(() => {
-  //   console.log(testList)
-  //   console.log(newSortComments)
-  // })
 
   return (
     <div>
@@ -93,8 +86,6 @@ export const Success = ({ answerComments, answerId }) => {
           <AnswerCommentCell key={item.id} id={item.id} answerId={answerId} />
         )
       })}
-      {/* Þarf ekki vegna useEffect */}
-      {/* <button onClick={sortComments}>Show comments</button> */}
     </div>
   )
 }
