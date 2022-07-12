f97d8d1bf132fc45a61228b8ec3e7e80cf80f2e6
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-07-12 15:56:03 +0000

    Trjáreiknirit optimizað í O(n) tíma og commentin nota nú það reiknirit

--- web/src/components/AnswerCommentsCell/AnswerCommentsCell.js
+++ web/src/components/AnswerCommentsCell/AnswerCommentsCell.js
@@ -18,10 +18,6 @@ export const QUERY = gql`
   }
 `
 
-const TreeModel = require('tree-model'),
-  tree = new TreeModel(),
-  root = tree.parse({ id: 0 })
-
 export const Loading = () => <div>Loading...</div>
 
 export const Empty = () => <div>Empty</div>
@@ -30,14 +26,21 @@ export const Failure = ({ error }) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
+/*
+ * Reikniritið byggir upp net með
+ * key-value adjacency list og
+ * ítrar í gegnum það með DFS
+ * til að skila kommentum í rétta röð
+ *
+ */
 const newSortComments = (answerComments) => {
   // Stilla hakkatöflu
   const hashMap = new Map()
   answerComments.forEach((item) => {
     try {
-      hashMap.set(item.parentId, [...hashMap.get(item.parentId), item.id])
+      hashMap.set(item.parentId, [...hashMap.get(item.parentId), item])
     } catch {
-      hashMap.set(item.parentId, [item.id])
+      hashMap.set(item.parentId, [item])
     }
   })
   console.log(hashMap)
@@ -51,101 +54,40 @@ const newSortComments = (answerComments) => {
   let currentNode
   let returnList = []
 
-  //Debug counter
-  let counter = 0
-
   // Ítra í gegn
   while (stack.length !== 0) {
     currentNode = stack.pop()
     returnList.push(currentNode)
-    currentChildren = hashMap.get(currentNode)
+    currentChildren = hashMap.get(currentNode.id)
     console.log(currentChildren)
     try {
       currentChildren.forEach((item) => stack.push(item))
-    } catch {}
-
-    console.log(returnList)
-    //Debug
-    counter++
-    if (counter > 20) return
+    } catch {
+      /* Engin börn */
+    }
   }
   console.log(returnList)
-}
-
-const sortComments = (answerComments) => {
-  //Clone-a answerComments fylkið
-  console.log(answerComments)
-  const tempList = [...answerComments]
-  console.log(tempList)
-  // Nýjustu commentin koma fyrst þegar búið er að reverse-a
-  let reversedList = tempList.reverse()
-  console.log(reversedList)
-
-  let childrenAdded = 0
-  let idQueue = []
-  let parentId = 0
-  let finalList = []
-  let currentTreeNode = root
-  let nodeQueue = []
-  while (childrenAdded !== reversedList.length) {
-    reversedList.forEach((item) => {
-      // console.log(item)
-      if (item.parentId == parentId) {
-        idQueue.push(item)
-        finalList.push(item)
-        let mutableItem = { ...item }
-        let newNode = tree.parse(mutableItem)
-        currentTreeNode.addChild(newNode)
-        nodeQueue.push(newNode)
-        console.log(item)
-        console.log(parentId)
-        childrenAdded++
-      }
-    })
-    console.log('childrenAdded: ' + childrenAdded)
-    console.log('list length ' + reversedList.length)
-    console.log(childrenAdded === reversedList.length)
-
-    currentTreeNode = nodeQueue[0]
-    console.log(currentTreeNode)
-    nodeQueue.shift()
-
-    console.log(idQueue)
-    parentId = idQueue[0].id
-    console.log('parentId: ' + parentId)
-    idQueue.shift()
-    console.log(idQueue)
-  }
-  console.log(finalList)
-  console.log(root)
-
-  let returnList = []
-
-  root.walk((node) => {
-    node.model.children = null
-    console.log(node.model)
-    if (node.model.id !== 0) {
-      returnList.push(node.model)
-    }
-  })
-
   return returnList
 }
 
 export const Success = ({ answerComments, answerId }) => {
   const [list, setList] = useState([])
+  // const testList = useRef(newSortComments(answerComments))
 
   //useEffect(() => console.log(answerComments))
 
   useLayoutEffect(() => {
-    setList(sortComments(answerComments))
+    setList(newSortComments(answerComments))
   }, [answerComments])
+
+  // useEffect(() => {
+  //   console.log(testList)
+  //   console.log(newSortComments)
+  // })
 
   return (
     <div>
-      <button onClick={() => newSortComments(answerComments)}>
-        New Sort comments
-      </button>
+      <button onClick={() => testSort()}>New Sort comments</button>
       {list.map((item) => {
         return (
           <AnswerCommentCell key={item.id} id={item.id} answerId={answerId} />
