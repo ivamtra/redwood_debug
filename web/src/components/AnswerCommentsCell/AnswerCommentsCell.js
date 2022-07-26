158bb64c5e92f817ae42caedbe805f8d6fa607fd
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-07-26 17:52:29 +0000

    TestNotification deletað, answercommentsCell refactorað, commentSort sett í sér file í undirbúningi fyrir questionComment

--- web/src/components/AnswerCommentsCell/AnswerCommentsCell.js
+++ web/src/components/AnswerCommentsCell/AnswerCommentsCell.js
@@ -26,53 +26,6 @@ export const Failure = ({ error }) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
-/*
- * Reikniritið byggir upp net með
- * key-value adjacency list og
- * ítrar í gegnum það með DFS
- * til að skila kommentum í rétta röð
- *
- * Tímaflækja O(n)
- */
-const newSortComments = (answerComments) => {
-  // Stilla hakkatöflu
-  // Ætti að vera O(n) ef append er O(1)
-  const hashMap = new Map()
-  answerComments.forEach((item) => {
-    try {
-      hashMap.set(item.parentId, [...hashMap.get(item.parentId), item])
-    } catch {
-      hashMap.set(item.parentId, [item])
-    }
-  })
-  console.log(hashMap)
-
-  // Upphafsstilling
-  const stack = []
-  let currentChildren = hashMap.get(0)
-  currentChildren.forEach((child) => {
-    stack.push(child)
-  })
-  console.log(stack)
-  let currentNode
-  let returnList = []
-
-  // Ítra í gegn með DFS
-  while (stack.length !== 0) {
-    currentNode = stack.pop()
-    returnList.push(currentNode)
-    currentChildren = hashMap.get(currentNode.id)
-    console.log(currentChildren)
-    try {
-      currentChildren.forEach((item) => stack.push(item))
-    } catch {
-      /* Engin börn */
-    }
-  }
-  console.log(returnList)
-  return returnList
-}
-
 export const Success = ({ answerComments, answerId }) => {
   const [list, setList] = useState([])
 
