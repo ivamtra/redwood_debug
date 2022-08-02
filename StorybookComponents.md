66611437dae538f16f4b206f0ef98ab44bc9feaf
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-08-02 15:25:12 +0000

    sb componentar lagaðir

---
+++ StorybookComponents.md
@@ -0,0 +1,22 @@
+# Virka
+## Cells
+- Answer/s
+- Question
+- AnswerComment
+- AnswerComments
+- Questions
+- Sentence/s
+- Translation/s
+- User
+## Forms
+- AnswerCommentForm
+- AnswerForm
+- NewQuestionForm
+## Takkar
+- DeleteButton
+- Flagbutton
+- HideButton
+
+# Virka ekki
+
+- Notification/s
--- web/src/components/AnswerCell/AnswerCell.mock.js
+++ web/src/components/AnswerCell/AnswerCell.mock.js
@@ -1,6 +1,16 @@
 // Define your own mock data here:
 export const standard = () => ({
   answer: {
-    id: 42,
+    id: 0,
+    title: 'Placeholder',
+    justification: 'Placeholder',
+    userId: 0,
+    createdAt: '2022-07-28T18:41:12.830Z',
+    user: {
+      email: 'Email',
+    },
+    questionId: 0,
+    rating: 0,
+    isHidden: false,
   },
 })
