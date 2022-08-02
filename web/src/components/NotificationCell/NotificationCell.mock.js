b52c76486f605eac77ff90c858a5a8dd91293fd2
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-08-02 15:30:31 +0000

    sb virkar nu fyrir alla componenta

--- web/src/components/NotificationCell/NotificationCell.mock.js
+++ web/src/components/NotificationCell/NotificationCell.mock.js
@@ -1,6 +1,13 @@
 // Define your own mock data here:
 export const standard = () => ({
   notification: {
-    id: 42,
+    id: 13,
+    body: 'User með id 4 svaraði athugasemd þinni',
+    isSeen: false,
+    questionId: 3,
+    answerId: 0,
+    answerCommentId: 29,
+    userId: 3,
+    createdAt: '2022-07-28T20:25:49.946Z',
   },
 })
